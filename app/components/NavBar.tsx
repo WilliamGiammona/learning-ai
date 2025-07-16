596ab3673304befc42214a23da5e3573aa0332b1
William Giammona <wgiammona@gmail.com>
2025-07-16 17:07:37 +0300

    updated link

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -20,10 +20,10 @@ export default function Navbar() {
               Home
             </Link>
             <Link
-              href="/tutorials"
+              href="/fast-ai"
               className="text-gray-700 hover:text-blue-600 transition-colors duration-200"
             >
-              Tutorials
+              Fast-ai
             </Link>
             <Link
               href="/courses"
