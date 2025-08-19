bd1d8ff09729a8918c2cbb7ec6825181db874a89
William Giammona <wgiammona@gmail.com>
2025-08-19 17:21:58 +0300

    adding overview text

--- app/robotics/page.tsx
+++ app/robotics/page.tsx
@@ -8,7 +8,16 @@ const page = () => {
       </h1>
 
       <div className="space-y-4 text-gray-700 leading-relaxed">
-        <p>test</p>
+        <h3 className="text-xl font-semibold">Overview</h3>
+        <p>
+          MuJoCo is a program that simulates the laws of physics. To tell it
+          what my environment/robot should look like, I must write it a text
+          file (XML). Writing XML files to specify environments is a type of
+          format called MJCF. A popular robotics format called URDF is also
+          acceptable. MuJoCo comes with a built in viewer window to see your
+          robot moving around.
+        </p>
+        <p></p>
       </div>
     </div>
   );
