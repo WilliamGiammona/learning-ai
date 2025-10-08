65b358293cf31c4409501a14b396e9c70f2e2f6a
William Giammona <wgiammona@gmail.com>
2025-10-08 19:55:42 +0300

    reinstated globals css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-// import "./globals.css";
+import "./globals.css";
 import Navbar from "@/app/components/NavBar";
 
 export const metadata: Metadata = {
