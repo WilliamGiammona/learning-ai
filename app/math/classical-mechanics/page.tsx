a2c29ef8bbd3e67e78174fd0d1215cbcc03e981f
William Giammona <wgiammona@gmail.com>
2025-10-15 19:22:53 +0300

    name change

--- app/math/classical-mechanics/page.tsx
+++ app/math/classical-mechanics/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { LaTeX } from "@/app/components/LaTeX"; // Adjust path as needed
 
-const Page = () => {
+const ClassicalMechanics = () => {
   return (
     <section className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <section className="grid grid-cols-1 md:grid-cols-12 gap-8">
@@ -587,4 +587,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default ClassicalMechanics;
