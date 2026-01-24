a32b4bf297154063134bda61a6d4f35d2d5bb63f
William Giammona <wgiammona@gmail.com>
2026-01-24 12:53:58 +0200

    continuing mdp section

--- app/reinforcement-learning/page.tsx
+++ app/reinforcement-learning/page.tsx
@@ -1804,8 +1804,9 @@ export default function ReinforcementLearningPage() {
               equation we saw earlier for evaluating a specific policy, because
               once you fix a policy an MDP can be reduced to an MRP (you can
               derive values from states only, instead of state-action pairs).
-              However, usually we want to know the state-action value pairs
-              instead of just the state values, which will be explained later.
+              However, usually we don't want to compress MDPs into MRPs because
+              we want to know the state-action value pairs instead of just the
+              state values. The reasons why will be explained later.
             </p>
 
             <p className="mb-4">
