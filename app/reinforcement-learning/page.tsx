9e32b875690b14c088d8d9fe483932e013a2f6af
William Giammona <wgiammona@gmail.com>
2026-01-25 16:53:29 +0200

    continuing dynamic programming section

--- app/reinforcement-learning/page.tsx
+++ app/reinforcement-learning/page.tsx
@@ -200,7 +200,7 @@ export default function ReinforcementLearningPage() {
                   </li>
                   <li>
                     <a
-                      href="#dp-intro"
+                      href="#dp-history"
                       className="text-blue-600 hover:text-blue-800 underline block"
                     >
                       History of Dynamic Programming
