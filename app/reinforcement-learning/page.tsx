8c43fc0b984e54ddc9ef13bcb72cd0b14026934c
William Giammona <wgiammona@gmail.com>
2026-01-27 17:38:50 +0200

    continuing dynamic programming section

--- app/reinforcement-learning/page.tsx
+++ app/reinforcement-learning/page.tsx
@@ -216,7 +216,7 @@ export default function ReinforcementLearningPage() {
                   </li>
                   <li>
                     <a
-                      href="#dp-policy-iteraton"
+                      href="#dp-policy-iteration"
                       className="text-blue-600 hover:text-blue-800 underline block"
                     >
                       Policy Iteration
