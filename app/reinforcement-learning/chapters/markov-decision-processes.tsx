1bb5b11fc8cf8c0c8b50193749a967dbe0e2c339
William Giammona <wgiammona@gmail.com>
2026-01-29 17:46:12 +0200

    initial commit

---
+++ app/reinforcement-learning/chapters/markov-decision-processes.tsx
@@ -0,0 +1,15 @@
+export default function MarkovDecisionProcess() {
+  return (
+    <section id="ch-four-model-free-prediction" className="mb-12">
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        Model Free Prediction
+      </h1>
+
+      <h2 id="mfp-intro" className="text-2xl font-bold mb-6 text-center">
+        Introduction
+      </h2>
+
+      <div className="mb-4">test</div>
+    </section>
+  );
+}
