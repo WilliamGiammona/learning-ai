590109d423d2aaf462df714d5f7e9972fae4827f
William Giammona <wgiammona@gmail.com>
2026-01-29 17:51:14 +0200

    initial commit

---
+++ app/reinforcement-learning/chapters/dynamic-programming.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import "katex/dist/katex.min.css";
+import { InlineMath, BlockMath } from "react-katex";
+
+export default function DynamicProgramming() {
+  return (
+    <section id="ch-four-model-free-prediction" className="mb-12">
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        Model Free Prediction
+      </h1>
+
+      <h2 id="mfp-intro" className="text-2xl font-bold mb-6 text-center">
+        Introduction
+      </h2>
+
+      <div className="mb-4">test</div>
+    </section>
+  );
+}
