127361e886230ade50ebec72af4b779cba3dda01
William Giammona <wgiammona@gmail.com>
2026-02-01 16:49:01 +0200

    continuing monte carlo learning section

--- app/reinforcement-learning/chapters/model-free-prediction.tsx
+++ app/reinforcement-learning/chapters/model-free-prediction.tsx
@@ -93,40 +93,10 @@ export default function ModelFreePrediction() {
       </figure>
 
       <div className="mb-4">
-        The story of Monte Carlo methods doesn&apos;t start in a casino.
-        <br />
-        <br />
-        It starts in a hospital bed.
-        <br />
-        <br />
-        In the mid-1940s, Polish mathematician Stanisław Ulam was working at Los
-        Alamos as part of the Manhattan Project.
-        <br />
-        <br />
-        One of the problems on the table was terrifyingly complex:
-        <br />
-        <br />
-        <em>How do neutrons behave inside a nuclear core?</em>
-        <br />
-        <br />
-        There are trillions of neutrons.
-        <br />
-        <br />
-        Each one collides, scatters, gets absorbed, or triggers fission.
-        <br />
-        <br />
-        And each interaction depends on what just happened before.
-        <br />
-        <br />
-        Writing down exact equations for all of that was completely impossible
-        with the mathematics and computing power of the 1940s.
-        <br />
-        <br />
-        Then Ulam got sick.
-        <br />
-        <br />
-        While recovering from surgery, he spent long stretches in bed, killing
-        time by playing solitaire.
+        In the mid-1940s, Polish mathematician Stanislaw Ulam was working at Los
+        Alamos as part of the Manhattan Project. He got encephalitis
+        (inflammation of the brain), and spent a lot of the time recovering in
+        his hospital bed playing solitaire.
         <br />
         <br />
         At some point, a simple question popped into his head:
@@ -138,86 +108,39 @@ export default function ModelFreePrediction() {
         </em>
         <br />
         <br />
-        This turns out to be a brutally hard problem.
+        The number of possible card arrangements is 52 factorial, and so Ulam
+        realized that computing the exact answer was hopeless.
         <br />
         <br />
-        The number of possible card arrangements is 52 factorial.
-        <br />
-        <br />
-        That&apos;s a number so large that even writing it down feels rude.
-        <br />
-        <br />
-        Ulam realized that computing the exact answer was hopeless.
-        <br />
-        <br />
-        But there was another option.
-        <br />
-        <br />
-        He could just play.
-        <br />
-        <br />
-        Shuffle the cards.
-        <br />
-        <br />
-        Play a game.
-        <br />
-        <br />
-        Record whether it was winnable.
-        <br />
-        <br />
-        Repeat this hundreds or thousands of times.
+        But there was another option, he could just shuffle the cards, play a
+        game, record whether it was winnable, and repeat this hundreds or
+        thousands of times.
         <br />
         <br />
         The fraction of games that could be won wouldn&apos;t give the exact
-        answer — but it would give a <em>statistical approximation</em>.
+        answer, but it would give a <em>statistical approximation</em>.
         <br />
         <br />
-        Close enough to be useful.
+        When Ulam returned to Los Alamos, he decided to use the same method to
+        try to figure out how neutrons behave inside a nuclear core (there are
+        trillions of neutrons all interacting with their surroundings), too many
+        to calculate back in the 1940s.
         <br />
         <br />
-        When Ulam returned to Los Alamos, he realized something important.
+        Ulam discussed the idea with John von Neumann, but they both knew that
+        unlike solitaire games which are independent, neutron interactions
+        inside a core are not. Each neutron&apos;s future activity depends on
+        its current state and what it just interacted with, so you couldn&apos;t
+        just sample random outcomes like in solitaire.
         <br />
         <br />
-        The neutron problem had the same shape.
+        As we have already learned, a Markov Process is the perfect mathematical
+        tool to apply when you want to use probability theory and statistics on
+        dependent events, and that&apos;s exactly what they decided to use.
         <br />
         <br />
-        Too many possibilities to enumerate.
-        <br />
-        <br />
-        Too many interactions to compute exactly.
-        <br />
-        <br />
-        But maybe you didn&apos;t need the exact answer.
-        <br />
-        <br />
-        Maybe you could simulate lots of random outcomes and let statistics do
-        the work.
-        <br />
-        <br />
-        Ulam discussed the idea with John von Neumann.
-        <br />
-        <br />
-        Von Neumann immediately saw both the promise and the catch.
-        <br />
-        <br />
-        Solitaire games are independent.
-        <br />
-        <br />
-        Neutrons are not.
-        <br />
-        <br />
-        Each neutron&apos;s future depends on its current state and what it just
-        interacted with.
-        <br />
-        <br />
-        In other words, this wasn&apos;t just random sampling.
-        <br />
-        <br />
-        It was a problem tailor-made for a Markov process.
-        <br />
-        <br />
-        Together with colleagues, they implemented the idea on one of the
-        world&apos;s first electronic computers: the ENIAC.
+        Together, they implemented the idea on one of the world&apos;s first
+        electronic computers: the ENIAC.
         <br />
         <br />
         Instead of solving equations directly, the computer generated huge
@@ -230,11 +153,9 @@ export default function ModelFreePrediction() {
         And it needed a name.
         <br />
         <br />
-        The heavy use of randomness and probability reminded Ulam of the most
-        famous gambling city in the world.
-        <br />
-        <br />
-        Monte Carlo.
+        An urban legend is that the heavy use of randomness and probability
+        reminded Ulam of the Monte Carlo Casino, where his uncle spent a lot of
+        time.
         <br />
         <br />
         At its core, the Monte Carlo idea is simple:
@@ -242,15 +163,9 @@ export default function ModelFreePrediction() {
         <br />
         <em>
           If you can&apos;t compute the expectation directly, sample the world
-          and estimate it statistically.
+          and estimate it statistically. This is one of the methods used in
+          reinforcement learning to find value functions.
         </em>
-        <br />
-        <br />
-        That idea is about to show up again.
-        <br />
-        <br />
-        This time, instead of neutrons or cards, we&apos;ll be sampling
-        trajectories in a reinforcement learning environment.
       </div>
     </section>
   );
