30b191ae7f097e7a1ed52a6812505da614760390
William Giammona <wgiammona@gmail.com>
2026-02-06 18:07:29 +0200

    initial commit

---
+++ app/reinforcement-learning/chapters/model-free-control.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+import "katex/dist/katex.min.css";
+import { InlineMath, BlockMath } from "react-katex";
+
+export default function ModelFreeControl() {
+  return (
+    <section id="ch-five-model-free-control" className="mb-12">
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        Model Free Control
+      </h1>
+
+      <h2 id="mfc-intro" className="text-2xl font-bold mb-6 text-center">
+        Introduction
+      </h2>
+    </section>
+  );
+}
